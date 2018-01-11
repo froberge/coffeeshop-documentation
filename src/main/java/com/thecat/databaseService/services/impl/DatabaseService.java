d83c4910507af571eef423190e5a077759491015
Felix Roberge <roberge.felix@gmail.com>
2018-01-11 13:11:51 -0500

    add code to query database

--- src/main/java/com/thecat/databaseService/services/impl/DatabaseService.java
+++ src/main/java/com/thecat/databaseService/services/impl/DatabaseService.java
@@ -6,12 +6,15 @@ package com.thecat.databaseService.services.impl;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+
+import org.apache.http.ConnectionClosedException;
 
 import com.thecat.databaseService.entities.User;
 import com.thecat.databaseService.entities.UserJson;
@@ -53,13 +56,15 @@ public class DatabaseService {
 	 * @return {@link User}
 	 */
 	public User select(String emailAdress, String password) {
-		
-		try {
+
+		try {		
 			Connection connection = connectToDatabase();
-			
+
 			if ( connection != null ) {
 				System.out.println( "connection sucessful" );
-				
+
+				createaUserTable( connection );
+							
 				String sql = "select * from USERS";
 				Statement stmt = connection.createStatement();
 				ResultSet rs = stmt.executeQuery(sql);
@@ -68,10 +73,10 @@ public class DatabaseService {
 				
 				while (rs.next() ) {
 					System.out.println( "check element name: " + rs.getString( "NAME" ) );
-					
 				}
 				
 				rs.close();
+				connection.close();
 				 
 			} else {
 				System.out.println( "no connection" );
@@ -139,6 +144,12 @@ public class DatabaseService {
 	        user.getPassword()));
 	}
 	
+	/**
+	 *  This method create a connection to the databse.
+	 *  
+	 * @return {@link Connection}
+	 * @throws Exception
+	 */
 	private Connection connectToDatabase() throws Exception{
 		StringBuffer dbUrl = new StringBuffer( "jdbc:postgresql://" );
 		dbUrl.append( System.getenv( "POSTGRESQL_SERVICE_HOST" ) );
@@ -162,5 +173,35 @@ public class DatabaseService {
 			throw e;
 		}
 	}
-	
+
+	/**
+	 * CMethod that create a new table and a sequence into the database for the user.
+	 * 
+	 * @param connection {@link Connection}
+	 * @throws SQLException
+	 */
+	private void createaUserTable(Connection connection) throws SQLException {
+		Statement stmt = connection.createStatement();
+		String createSequence = "CREATE SEQUENCE users_seq "
+				+ "START WITH 1 "
+				+ "INCREMENT BY   1";
+		
+		stmt.executeUpdate(createSequence);
+		System.out.println( "sequence is created" );
+		
+		
+		String createTable = "CREATE TABLE USERS " +
+			    "(ID INT NOT NULL, " +
+			    "NAME VARCHAR(100) NOT NULL, " +
+			    "GENDER VARCHAR(6) NOT NULL, " +
+			    "BIRTHDATE DATE, " +
+			    "EMAILADR VARCHAR(100) NOT NULL, " +
+			    "PASSWORD  VARCHAR(20)  NOT NULL, " +
+			    "CREATE_DATE DATE, " + 
+			    "PRIMARY KEY (ID))";
+
+		
+		stmt.executeUpdate(createTable);
+		System.out.println( "table is created" );
+	}
 }
