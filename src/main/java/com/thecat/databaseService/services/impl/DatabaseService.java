0d025dc70b637cc5d7414de59e4a7ac25dd3414d
Felix Roberge <roberge.felix@gmail.com>
2018-09-04 15:28:38 -0400

    remove the possibility to use a static list instead of the database

--- src/main/java/com/thecat/databaseService/services/impl/DatabaseService.java
+++ src/main/java/com/thecat/databaseService/services/impl/DatabaseService.java
@@ -9,14 +9,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
 
 import com.thecat.databaseService.entities.User;
 import com.thecat.databaseService.entities.UserJson;
@@ -31,14 +25,10 @@ public class DatabaseService {
 
 	private static DatabaseService dbService = null;
 	
-	private List<User> userList;
-	
 	/**
 	 * Private constructor to prevent the creation of the DatabaseService
 	 */
 	private DatabaseService(){
-		// Initialize the userList
-		this.userList = createUserList();
 	}
 	
 	public static DatabaseService getInstance() {
@@ -58,9 +48,7 @@ public class DatabaseService {
 	 * @return {@link User}
 	 */
 	public User select(String emailAdress, String password) {
-		User response = selectUserFromList(emailAdress, password);
-		
-//		User response = selectUserFromDatabase(emailAdress, password);
+		User response = selectUserFromDatabase(emailAdress, password);
 		
 		return response;
 	}
@@ -106,18 +94,6 @@ public class DatabaseService {
 	}
 
 	/**
-	 * Select user from the list
-	 * 
-	 * @param emailAdress {@link String}
-	 * @param password {@link String}
-	 * @return {@link UserJson}
-	 */
-	private User selectUserFromList(String emailAdress, String password) {
-		return this.userList.stream().filter( u -> ( u.getEmailAddress().equals(emailAdress)
-			&& u.getPassword().equals(password) ) ).findFirst().orElse(null);
-	}
-	
-	/**
 	 * Add a new user for a system
 	 * 
 	 * @param user {@link UserJson}
@@ -125,49 +101,11 @@ public class DatabaseService {
 	 */
 	public boolean register(UserJson user) {
 		
-//		insertUserInList(user);
-
 		insertUserInDatabase(user);
 				
 		return true;
 	}
 
-	
-
-	/**
-	 * Create a basic list of customer instead of using a database.
-	 * 
-	 * @return {@link ArrayList}
-	 */
-	private List<User> createUserList() {
-	    List<User> list = new LinkedList<User>();
-	    list.add(
-	        new User(
-	        "default",
-	        User.Gender.MALE,
-	        LocalDate.now().minusYears(17),
-	        "default@example.com",
-	        "default123"));
-	    
-	    return list;
-	}
-	
-	/**
-	 * Insert a new user in the list.
-	 * 
-	 * @param user {@link UserJson}
-	 */
-	private void insertUserInList(UserJson user) {
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-		
-		this.userList.add(
-	        new User(
-	        user.getUsername(),
-	        User.Gender.valueOf(user.getGender().toUpperCase()),
-	        LocalDate.parse(user.getAge(), formatter),
-	        user.getEmailAdr(),
-	        user.getPassword()));
-	}
 	
 	/**
 	 *  This method create a connection to the database.
@@ -270,43 +208,4 @@ public class DatabaseService {
 
 		return response;
 	}
-
-
-/*	private void createDatabase() throws SQLException {
-
-		try {
-			Connection connection = getDatabaseConnection();
-
-			if ( connection != null ) {
-				Statement stmt = connection.createStatement();
-				String createSequence = "CREATE SEQUENCE users_seq "
-										+ "START WITH 1 "
-										+ "INCREMENT BY   1";
-		
-				stmt.executeUpdate(createSequence);
-				System.out.println( "sequence is created" );
-		
-		
-				String createTable = "CREATE TABLE USERS " +
-									"(ID INT NOT NULL, " +
-									"NAME VARCHAR(100) NOT NULL, " +
-									"GENDER VARCHAR(6) NOT NULL, " +
-									"BIRTHDATE DATE, " +
-									"EMAILADR VARCHAR(100) NOT NULL, " +
-									"PASSWORD  VARCHAR(20)  NOT NULL, " +
-									"CREATE_DATE DATE, " +
-									"PRIMARY KEY (ID))";
-
-				stmt.executeUpdate(createTable);
-				System.out.println( "table is created" );
-
-				connection.close();
-			} else {
-				System.out.println( "no connection" );
-			}
-		} catch (Exception e ) {
-			System.out.println( e );
-	}
-
-*/
 }
