9ec338a045189ef16a529210edc6baa5c8460afd
Felix Roberge <roberge.felix@gmail.com>
2021-05-04 18:09:17 -0400

    commit the database changes

--- dbscripts/users-schema/createInsertUser.sql
+++ dbscripts/users-schema/createInsertUser.sql
@@ -1,13 +1,14 @@
-DROP TABLE IF EXISTS USERS;
-DROP SEQUENCE IF EXISTS users_seq;
+DROP SEQUENCE IF EXISTS cs_user.user_seq;
+DROP TABLE IF EXISTS cs_user."user";
+DROP SCHEMA IF EXISTS cs_user;
 
-BEGIN;
+CREATE SCHEMA cs_user;
 
-CREATE SEQUENCE users_seq
+CREATE SEQUENCE cs_user.user_seq
   START WITH     1
   INCREMENT BY   1;
-
-CREATE TABLE USERS(
+ 
+CREATE TABLE cs_user."user"(
   ID   INT                NOT NULL,
   NAME VARCHAR(100)       NOT NULL,
   GENDER VARCHAR(6)       NOT NULL,
@@ -18,9 +19,10 @@ CREATE TABLE USERS(
   PRIMARY KEY (ID)
 );
 
-INSERT INTO USERS (ID, NAME, GENDER, BIRTHDATE, EMAILADR, PASSWORD, CREATE_DATE) VALUES
-  (nextval('users_seq'), 'System Admin','Male', current_timestamp, 'admin@coffeeshop.com', 'pwd001',current_timestamp );
-INSERT INTO USERS (ID, NAME, GENDER, BIRTHDATE, EMAILADR, PASSWORD, CREATE_DATE) VALUES
-  (nextval('users_seq'), 'Default user','Male', current_timestamp, 'default@coffeeshop.com', 'default123',current_timestamp );
+INSERT INTO cs_user."user" (ID, NAME, GENDER, BIRTHDATE, EMAILADR, PASSWORD, CREATE_DATE) VALUES
+  (nextval('cs_user.user_seq'), 'System Admin','Male', current_timestamp, 'admin@coffeeshop.com', 'pwd001',current_timestamp );
+INSERT INTO cs_user."user" (ID, NAME, GENDER, BIRTHDATE, EMAILADR, PASSWORD, CREATE_DATE) VALUES
+  (nextval('cs_user.user_seq'), 'Default user','Male', current_timestamp, 'default@coffeeshop.com', 'default123',current_timestamp );
 
-COMMIT;
+SET default_tablespace = '';
+SET client_encoding = 'UTF8';
